f3bf102658766b1447ac3b5885be6bd9fec636b7
peastman <peastman@8e91ba97-c92a-0410-817f-f646a3b87f38>
2008-07-19 19:28:41 +0000

    Added accessor methods to TextureImageInfo
    git-svn-id: svn://svn.code.sf.net/p/aoi/code@148 8e91ba97-c92a-0410-817f-f646a3b87f38

--- Translators/src/artofillusion/translators/TextureImageInfo.java
+++ Translators/src/artofillusion/translators/TextureImageInfo.java
@@ -4,15 +4,17 @@
    terms of the GNU General Public License as published by the Free Software
    Foundation; either version 2 of the License, or (at your option) any later version.
 
-   This program is distributed in the hope that it will be useful, but WITHOUT ANY 
-   WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
+   This program is distributed in the hope that it will be useful, but WITHOUT ANY
+   WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
    PARTICULAR PURPOSE.  See the GNU General Public License for more details. */
 
 package artofillusion.translators;
 
 import artofillusion.texture.*;
 
-/** This class is used by TextureImageExporter for storing information about texture images. */
+/**
+ * This class is used by TextureImageExporter for storing information about texture images.
+ */
 
 public class TextureImageInfo
 {
@@ -21,7 +23,7 @@ public class TextureImageInfo
   String diffuseFilename, specularFilename, hilightFilename, transparentFilename, emissiveFilename, bumpFilename;
   double minu, minv, maxu, maxv;
   double paramValue[];
-  
+
   public TextureImageInfo(Texture tex, double param[])
   {
     texture = tex;
@@ -29,4 +31,109 @@ public class TextureImageInfo
     minu = minv = Double.MAX_VALUE;
     maxu = maxv = -Double.MAX_VALUE;
   }
+
+  /**
+   * @return The texture with which this TextureImageInfo is associated.
+   */
+  public Texture getTexture()
+  {
+    return texture;
+  }
+
+  /**
+   * @return The name of this TextureImageInfo.
+   */
+  public String getName()
+  {
+    return name;
+  }
+
+  /**
+   * @return The name of the file containing this TextureImageInfo's diffuse texture.
+   */
+  public String getDiffuseFilename()
+  {
+    return diffuseFilename;
+  }
+
+  /**
+   * @return The name of the file containing this TextureImageInfo's specular texture.
+   */
+  public String getSpecularFilename()
+  {
+    return specularFilename;
+  }
+
+  /**
+   * @return The name of the file containing this TextureImageInfo's hilight texture.
+   */
+  public String getHilightFilename()
+  {
+    return hilightFilename;
+  }
+
+  /**
+   * @return The name of the file containing this TextureImageInfo's transparency texture.
+   */
+  public String getTransparentFilename()
+  {
+    return transparentFilename;
+  }
+
+  /**
+   * @return The name of the file containing this TextureImageInfo's emissive texture.
+   */
+  public String getEmissiveFilename()
+  {
+    return emissiveFilename;
+  }
+
+  /**
+   * @return The name of the file containing this TextureImageInfo's bump texture.
+   */
+  public String getBumpFilename()
+  {
+    return bumpFilename;
+  }
+
+  /**
+   * @return The minimum U coordinate.
+   */
+  public double getMinU()
+  {
+    return minu;
+  }
+
+  /**
+   * @return The minimum V coordinate.
+   */
+  public double getMinV()
+  {
+    return minv;
+  }
+
+  /**
+   * @return The maximum U coordinate.
+   */
+  public double getMaxU()
+  {
+    return maxu;
+  }
+
+  /**
+   * @return The maximum V coordinate.
+   */
+  public double getMaxV()
+  {
+    return maxv;
+  }
+
+  /**
+   * @return Texture parameter values.
+   */
+  public double[] getParamValues()
+  {
+    return paramValue;
+  }
 }
+

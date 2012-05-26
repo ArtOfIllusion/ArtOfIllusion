85e45d0059abf68aa11b51c70b2878e52d2f2d83
peastman <peastman@8e91ba97-c92a-0410-817f-f646a3b87f38>
2012-05-26 19:02:21 +0000

    Avoid divide by zero when angle is 0
    git-svn-id: svn://svn.code.sf.net/p/aoi/code@348 8e91ba97-c92a-0410-817f-f646a3b87f38

--- ArtOfIllusion/src/artofillusion/animation/distortion/BendDistortion.java
+++ ArtOfIllusion/src/artofillusion/animation/distortion/BendDistortion.java
@@ -120,58 +120,60 @@ public class BendDistortion extends Distortion
     double theta = angle*(Math.PI/180.0);
     double scale = theta/(max-min);
     double radius = (max-min)/theta;
-    Vec3 center;
-    if (axis == X_AXIS)
-      {
-        if (direction == Y_AXIS)
-          for (int i = 0; i < newvert.length; i++)
-            {
-              double a = scale*(newvert[i].x-min);
-              double b = newvert[i].y-origin.y-radius;
-              newvert[i].set(min-Math.sin(a)*b, origin.y+radius+Math.cos(a)*b, newvert[i].z);
-            }
-        else
-          for (int i = 0; i < newvert.length; i++)
-            {
-              double a = scale*(newvert[i].x-min);
-              double b = newvert[i].z-origin.z-radius;
-              newvert[i].set(min-Math.sin(a)*b, newvert[i].y, origin.z+radius+Math.cos(a)*b);
-            }
-      }
-    else if (axis == Y_AXIS)
-      {
-        if (direction == X_AXIS)
-          for (int i = 0; i < newvert.length; i++)
-            {
-              double a = scale*(newvert[i].y-min);
-              double b = newvert[i].x-origin.x-radius;
-              newvert[i].set(origin.x+radius+Math.cos(a)*b, min-Math.sin(a)*b, newvert[i].z);
-            }
-        else
-          for (int i = 0; i < newvert.length; i++)
-            {
-              double a = scale*(newvert[i].y-min);
-              double b = newvert[i].z-origin.z-radius;
-              newvert[i].set(newvert[i].x, min-Math.sin(a)*b, origin.z+radius+Math.cos(a)*b);
-            }
-      }
-    else
-      {
-        if (direction == X_AXIS)
-          for (int i = 0; i < newvert.length; i++)
-            {
-              double a = scale*(newvert[i].z-min);
-              double b = newvert[i].x-origin.x-radius;
-              newvert[i].set(origin.x+radius+Math.cos(a)*b, newvert[i].y, min-Math.sin(a)*b);
-            }
-        else
-          for (int i = 0; i < newvert.length; i++)
-            {
-              double a = scale*(newvert[i].z-min);
-              double b = newvert[i].y-origin.y-radius;
-              newvert[i].set(newvert[i].x, origin.y+radius+Math.cos(a)*b, min-Math.sin(a)*b);
-            }
-      }
+    if (Math.abs(theta) > 1e-10)
+    {
+      if (axis == X_AXIS)
+        {
+          if (direction == Y_AXIS)
+            for (int i = 0; i < newvert.length; i++)
+              {
+                double a = scale*(newvert[i].x-min);
+                double b = newvert[i].y-origin.y-radius;
+                newvert[i].set(min-Math.sin(a)*b, origin.y+radius+Math.cos(a)*b, newvert[i].z);
+              }
+          else
+            for (int i = 0; i < newvert.length; i++)
+              {
+                double a = scale*(newvert[i].x-min);
+                double b = newvert[i].z-origin.z-radius;
+                newvert[i].set(min-Math.sin(a)*b, newvert[i].y, origin.z+radius+Math.cos(a)*b);
+              }
+        }
+      else if (axis == Y_AXIS)
+        {
+          if (direction == X_AXIS)
+            for (int i = 0; i < newvert.length; i++)
+              {
+                double a = scale*(newvert[i].y-min);
+                double b = newvert[i].x-origin.x-radius;
+                newvert[i].set(origin.x+radius+Math.cos(a)*b, min-Math.sin(a)*b, newvert[i].z);
+              }
+          else
+            for (int i = 0; i < newvert.length; i++)
+              {
+                double a = scale*(newvert[i].y-min);
+                double b = newvert[i].z-origin.z-radius;
+                newvert[i].set(newvert[i].x, min-Math.sin(a)*b, origin.z+radius+Math.cos(a)*b);
+              }
+        }
+      else
+        {
+          if (direction == X_AXIS)
+            for (int i = 0; i < newvert.length; i++)
+              {
+                double a = scale*(newvert[i].z-min);
+                double b = newvert[i].x-origin.x-radius;
+                newvert[i].set(origin.x+radius+Math.cos(a)*b, newvert[i].y, min-Math.sin(a)*b);
+              }
+          else
+            for (int i = 0; i < newvert.length; i++)
+              {
+                double a = scale*(newvert[i].z-min);
+                double b = newvert[i].y-origin.y-radius;
+                newvert[i].set(newvert[i].x, origin.y+radius+Math.cos(a)*b, min-Math.sin(a)*b);
+              }
+        }
+    }
     if (postTransform != null)
       for (int i = 0; i < newvert.length; i++)
         postTransform.transform(newvert[i]);

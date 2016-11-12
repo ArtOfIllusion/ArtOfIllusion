fef0df6c835febf448562965326398ff89b8bac8
makiam <maxtemplaar@gmail.com>
2016-11-12 22:38:50 +0300

    Fixed copyright headers

--- ArtOfIllusion/src/artofillusion/RenderSetupDialog.java
+++ ArtOfIllusion/src/artofillusion/RenderSetupDialog.java
@@ -1,4 +1,5 @@
 /* Copyright (C) 1999-2015 by Peter Eastman
+   Changes copyright (C) 2016 by Maksim Khramov
 
    This program is free software; you can redistribute it and/or modify it under the
    terms of the GNU General Public License as published by the Free Software

2c8003726e08e9f51faef4d45490af91f5d47caf
Maksim Khramov <maxtemplaar@gmail.com>
2018-05-10 12:28:18 +0300

    remove unnecessary ctors

--- Filters/src/artofillusion/image/filter/GlowFilter.java
+++ Filters/src/artofillusion/image/filter/GlowFilter.java
@@ -1,4 +1,5 @@
 /* Copyright (C) 2003-2009 by Peter Eastman
+   Changes copyright (C) 2018 by Maksim Khramov
 
    This program is free software; you can redistribute it and/or modify it under the
    terms of the GNU General Public License as published by the Free Software
@@ -28,9 +29,6 @@ public class GlowFilter extends ImageFilter
 
   private static final String SHAPES[] = new String[] {CROSSHAIR, DIAGONAL, STAR, CIRCLE};
 
-  public GlowFilter()
-  {
-  }
 
   /** Get the name of this filter.*/
 
--- Filters/src/artofillusion/image/filter/NoiseReductionFilter.java
+++ Filters/src/artofillusion/image/filter/NoiseReductionFilter.java
@@ -1,4 +1,5 @@
 /* Copyright (C) 2004-2009 by Peter Eastman
+   Changes copyright (C) 2018 by Maksim Khramov
 
    This program is free software; you can redistribute it and/or modify it under the
    terms of the GNU General Public License as published by the Free Software
@@ -26,9 +27,7 @@ import java.io.*;
 
 public class NoiseReductionFilter extends ImageFilter
 {
-  public NoiseReductionFilter()
-  {
-  }
+
 
   /** Get the name of this filter.*/
 

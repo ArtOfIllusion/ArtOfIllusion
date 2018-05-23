dfe468d7a1d92382f500e7d2551dea6a7c951e9e
Maksim Khramov <maxtemplaar@gmail.com>
2018-05-23 17:41:43 +0300

    Messages UI

---
+++ ArtOfIllusion/src/artofillusion/ui/MessageBox.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright 2018 Veeam Software.
+ * 
+ * Created by Maksim Khramov
+ * Date: May 23, 2018.
+ */
+package artofillusion.ui;
+
+/**
+ *
+ * @author maksim.khramov
+ */
+public class MessageBox {
+    
+}

ae651bb0065ba340c3bb7da2fc4dbf484bbe9dad
Maksim Khramov <maxtemplaar@gmail.com>
2018-05-24 12:03:28 +0300

    Messages UI. Update

--- ArtOfIllusion/src/artofillusion/ui/Messages.java
+++ ArtOfIllusion/src/artofillusion/ui/Messages.java
@@ -1,15 +1,36 @@
 /*
- * Copyright 2018 Veeam Software.
+ * Copyright 2018.
  * 
  * Created by Maksim Khramov
  * Date: May 23, 2018.
  */
 package artofillusion.ui;
 
+import java.awt.Component;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author maksim.khramov
  */
 public final class Messages {
-    public static void error()
+    
+    private static final String TITLE = "Art Of Illusion";
+    
+    private static final Icon icon;
+    static {
+        icon = new ImageIcon(Thread.currentThread().getContextClassLoader().getResource("artofillusion/Icons/appIcon.png"));
+    }
+    
+    public static void error(Object message, Component owner) {
+        JOptionPane.showMessageDialog(owner, message, TITLE, JOptionPane.ERROR_MESSAGE);
+    }
+    
+    public static void error(Object message) {
+        error(message, (Component)null);
+    }
+    
+
 }
---
+++ Tests/src/artofillusion/ui/MessagesTest.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright 2018 Veeam Software.
+ * 
+ * Created by Maksim Khramov
+ * Date: May 24, 2018.
+ */
+package artofillusion.ui;
+
+import org.junit.Test;
+
+/**
+ *
+ * @author maksim.khramov
+ */
+public class MessagesTest {
+
+    
+    @Test
+    public void testHello() {
+        Messages.error("Hello Errors");
+    }
+}

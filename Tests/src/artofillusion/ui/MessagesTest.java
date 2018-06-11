2712861a6695d1e331d0b08c2362a0315b5c7bc0
makiam <maxtemplaar@gmail.com>
2018-06-11 23:12:04 +0300

    Test plain dialog

--- Tests/src/artofillusion/ui/MessagesTest.java
+++ Tests/src/artofillusion/ui/MessagesTest.java
@@ -10,7 +10,16 @@
 
 package artofillusion.ui;
 
+import artofillusion.ArtOfIllusion;
+import artofillusion.PluginRegistry;
+import buoy.widget.BDialog;
+import buoy.widget.BStandardDialog;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JOptionPane;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 /**
@@ -18,8 +27,19 @@ import org.junit.Test;
  * @author maksim.khramov
  */
 public class MessagesTest {
-
     
+    private static final Logger logger = Logger.getLogger(MessagesTest.class.getName());
+    
+    @BeforeClass
+    public static void setupClass() {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (ClassNotFoundException | IllegalAccessException | InstantiationException | UnsupportedLookAndFeelException ex) {
+            logger.log(Level.INFO, "Exception at test setup", ex);
+        }
+        PluginRegistry.registerResource("TranslateBundle", "artofillusion", ArtOfIllusion.class.getClassLoader(), "artofillusion", null);
+    }
+
     @Test
     public void testError() {
         Messages.error("Hello Errors");
@@ -38,5 +58,11 @@ public class MessagesTest {
     @Test
     public void testPlain() {
       JOptionPane.showMessageDialog(null, "Plain message", "", JOptionPane.PLAIN_MESSAGE);
+    }
+    
+    @Test
+    public void testPlainStyleDialog() {
+        BStandardDialog dlg = new BStandardDialog("Art Of Illusion", Translate.text("savePoseAsGesture"), BStandardDialog.PLAIN);
+        String name = dlg.showInputDialog(new BDialog(), null, "New Gesture");
     }
 }

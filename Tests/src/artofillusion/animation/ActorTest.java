a13083c091434dbb8b86e5a28bdbde02ffa6707e
makiam <maxtemplaar@gmail.com>
2018-07-07 10:02:45 +0300

    Actor test update

--- Tests/src/artofillusion/animation/ActorTest.java
+++ Tests/src/artofillusion/animation/ActorTest.java
@@ -37,6 +37,26 @@ public class ActorTest {
         Assert.assertEquals(1, actor.getNumGestures());
     }
     
+    @Test
+    public void testActorAddGesture() {
+        Vec3[] cp = new Vec3[2];
+        cp[0] = new Vec3();
+        cp[1] = Vec3.vz();
+        float[] smoothness = new float[2];
+        smoothness[0] = 0;
+        smoothness[1] = 0;
+        
+        Curve curve = new Curve(cp, smoothness, Mesh.NO_SMOOTHING, true);
+        Actor actor = new Actor(curve);
+        
+        actor.addGesture((Gesture)curve.getPoseKeyframe(), "Gesture Two");
+        
+        Assert.assertEquals(2, actor.getNumGestures());        
+        Assert.assertEquals(2, actor.gesture.length);
+        Assert.assertEquals(2, actor.gestureName.length);
+        Assert.assertEquals(2, actor.gestureID.length);
+    }
+    
     
     @Test
     public void test() throws IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException {

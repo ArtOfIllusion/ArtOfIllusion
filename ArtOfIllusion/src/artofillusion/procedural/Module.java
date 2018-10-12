1ad101301e3f618875e639966769632b69200b46
Maksim Khramov <maxtemplaar@gmail.com>
2018-10-12 13:26:17 +0300

    Procedure's read from stream test

--- ArtOfIllusion/src/artofillusion/procedural/Module.java
+++ ArtOfIllusion/src/artofillusion/procedural/Module.java
@@ -1,4 +1,5 @@
 /* Copyright (C) 2000-2011 by Peter Eastman
+   Changes copyright (C) 2018 by Maksim Khramov
 
    This program is free software; you can redistribute it and/or modify it under the
    terms of the GNU General Public License as published by the Free Software
@@ -330,8 +331,8 @@ public class Module
   {
     try
     {
-      Constructor con = getClass().getConstructor(new Class [] {Point.class});
-      return (Module) con.newInstance(new Object [] {new Point(bounds.x, bounds.y)});
+      Constructor<?> con = getClass().getConstructor(Point.class);
+      return (Module) con.newInstance(new Point(bounds.x, bounds.y));
     }
     catch (Exception ex)
     {

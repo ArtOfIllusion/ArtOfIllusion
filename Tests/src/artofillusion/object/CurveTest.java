1724e0604133e8d7e7f893a979a45d7d563ef24a
Maksim Khramov <maxtemplaar@gmail.com>
2018-10-19 16:28:03 +0300

    Curve tests

--- Tests/src/artofillusion/object/CurveTest.java
+++ Tests/src/artofillusion/object/CurveTest.java
@@ -10,10 +10,71 @@
 
 package artofillusion.object;
 
+import artofillusion.math.Vec3;
+import org.junit.Assert;
+import org.junit.Test;
+
 /**
  *
  * @author maksim.khramov
  */
 public class CurveTest {
     
+    @Test
+    public void testCreateCurve()
+    {
+        Curve curve = new Curve(new Vec3[0], new float[0], Mesh.APPROXIMATING, true);
+        Assert.assertNotNull(curve);
+        Assert.assertNotNull(curve.getVertices());
+        Assert.assertEquals(0, curve.getVertices().length);
+        Assert.assertEquals(0, curve.getSmoothness().length);
+        Assert.assertEquals(Mesh.APPROXIMATING, curve.getSmoothingMethod());
+        
+    }
+    
+    @Test
+    public void testCreateCurve2()
+    {
+        Curve curve = new Curve(new Vec3[] {new Vec3(), new Vec3()}, new float[] {0f, 1f}, Mesh.APPROXIMATING, false);
+        Assert.assertNotNull(curve);
+        Assert.assertNotNull(curve.getVertices());
+        Assert.assertEquals(2, curve.getVertices().length);
+        Assert.assertEquals(2, curve.getSmoothness().length);
+        Assert.assertEquals(Mesh.APPROXIMATING, curve.getSmoothingMethod());
+        Assert.assertFalse(curve.isClosed());
+        
+    }
+    
+    @Test
+    public void testCurveDuplicate()
+    {
+        Curve source = new Curve(new Vec3[] {new Vec3(), new Vec3()}, new float[] {0f, 1f}, Mesh.APPROXIMATING, false);        
+        Curve curve = (Curve)source.duplicate();
+        
+        Assert.assertNotEquals(source, curve);
+        Assert.assertNotNull(curve);
+        Assert.assertNotNull(curve.getVertices());
+        Assert.assertEquals(2, curve.getVertices().length);
+        Assert.assertEquals(2, curve.getSmoothness().length);
+        Assert.assertEquals(Mesh.APPROXIMATING, curve.getSmoothingMethod());
+        Assert.assertFalse(curve.isClosed());
+        
+    }
+    
+    @Test
+    public void testCurveCopy()
+    {
+        Curve source = new Curve(new Vec3[] {new Vec3(), new Vec3()}, new float[] {0f, 1f}, Mesh.APPROXIMATING, false);        
+        Curve curve = new Curve(new Vec3[0], new float[0], Mesh.APPROXIMATING, true);
+        curve.copyObject(source);
+        
+        Assert.assertNotEquals(source, curve);
+        Assert.assertNotNull(curve);
+        Assert.assertNotNull(curve.getVertices());
+        Assert.assertEquals(2, curve.getVertices().length);
+        Assert.assertEquals(2, curve.getSmoothness().length);
+        Assert.assertEquals(Mesh.APPROXIMATING, curve.getSmoothingMethod());
+        Assert.assertFalse(curve.isClosed());
+        
+    }
 }

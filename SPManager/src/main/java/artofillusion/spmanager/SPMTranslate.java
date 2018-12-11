fe99d4a55dd4545996a1435a82cdc3b992172af8
makiam <maxtemplaar@gmail.com>
2018-12-11 22:27:19 +0300

    Remove unused methods

--- SPManager/src/main/java/artofillusion/spmanager/SPMTranslate.java
+++ SPManager/src/main/java/artofillusion/spmanager/SPMTranslate.java
@@ -10,13 +10,11 @@
  */
 package artofillusion.spmanager;
 
-import java.awt.*;
-import java.awt.event.*;
-import java.text.*;
-import java.util.*;
 import buoy.event.*;
 import buoy.widget.*;
-//import artofillusion.ui.*;
+import java.text.*;
+import java.util.*;
+
 
 /**
  *  This class extends AoI Translate Class so that i) the spmanager properties
@@ -41,35 +39,6 @@ public class SPMTranslate
     public static void setLocale( Locale l )
     {
         locale = l;
-/*
-        File dir = new File( ModellingApp.PLUGIN_DIRECTORY );
-        if ( dir.exists() )
-        {
-            String[] files = dir.list();
-            for ( int i = 0; i < files.length; i++ )
-                if ( files[i].startsWith( "SPManager" ) )
-                {
-                    ZipFile zf = null;
-                    try
-                    {
-                        zf = new ZipFile( new File( ModellingApp.PLUGIN_DIRECTORY, files[i] ) );
-                    }
-                    catch ( IOException ex )
-                    {
-                        continue;
-                        // Not a zip file.
-                    }
-                    if ( zf != null )
-                    {
-                        JarClassLoader jcl = new JarClassLoader( zf );
-                        resources = ResourceBundle.getBundle( "spmanager", locale, jcl );
-                    }
-                }
-        }
-        else
-        {
-            System.out.println( "Dir does not exist" );
-        }*/
         resources = ResourceBundle.getBundle("spmanager", locale);
     }
 
@@ -85,40 +54,6 @@ public class SPMTranslate
         return locale;
     }
 
-
-    /**
-     *  Get a Menu whose text is given by the property "menu.(name)".
-     *
-     *@param  name  Description of the Parameter
-     *@return       Description of the Return Value
-     */
-
-    public static BMenu bMenu( String name )
-    {
-        try
-        {
-            return new BMenu( resources.getString( "menu." + name ) );
-        }
-        catch ( MissingResourceException ex )
-        {
-            return new BMenu( name );
-        }
-    }
-
-
-    /**
-     *  Get a BMenuItem whose text is given by the property "menu.(name)". This
-     *  checks for a property called "menu.shortcut.(name)", and if it is found,
-     *  sets the menu shortcut accordingly. No event link is added.
-     *
-     *@param  name  Description of the Parameter
-     *@return       Description of the Return Value
-     */
-
-    public static BMenuItem bMenuItem( String name )
-    {
-        return bMenuItem( name, null, null, null );
-    }
 
 
     /**
@@ -191,17 +126,6 @@ public class SPMTranslate
     }
 
 
-    /**
-     *  Get a BButton whose text is given by the property "button.(name)".
-     *
-     *@param  name  Description of the Parameter
-     *@return       Description of the Return Value
-     */
-
-    public static BButton bButton( String name )
-    {
-        return bButton( name, null, null, null );
-    }
 
 
     /**
@@ -242,111 +166,6 @@ public class SPMTranslate
             button.addEventLink( eventType, target, method );
         return button;
     }
-
-
-    /**
-     *  Get a BRadioButton whose text is given by the property "radio.(name)".
-     *  group is the RadioButtonGroup the radio button created button will
-     *  belong to.
-     *
-     *@param  name   Description of the Parameter
-     *@param  state  Description of the Parameter
-     *@param  group  Description of the Parameter
-     *@return        Description of the Return Value
-     */
-
-    public static BRadioButton bRadioButton( String name, boolean state, RadioButtonGroup group )
-    {
-        return bRadioButton( name, state, group, null, null );
-    }
-
-
-    /**
-     *  Description of the Method
-     *
-     *@param  name    Description of the Parameter
-     *@param  state   Description of the Parameter
-     *@param  group   Description of the Parameter
-     *@param  target  Description of the Parameter
-     *@param  method  Description of the Parameter
-     *@return         Description of the Return Value
-     */
-    public static BRadioButton bRadioButton( String name, boolean state, RadioButtonGroup group, Object target, String method )
-    {
-        String command = name;
-        try
-        {
-            command = resources.getString( "radio." + name );
-        }
-        catch ( MissingResourceException ex )
-        {
-        }
-        BRadioButton radio = new BRadioButton( command, state, group );
-        //radio.setText(command);
-        //radio.setState(state);
-        if ( target != null )
-            group.addEventLink( SelectionChangedEvent.class, target, method );
-        return radio;
-    }
-
-
-    /**
-     *  These functions are for compatibility with the AWT preview window
-     *
-     *@param  name      Description of the Parameter
-     *@param  listener  Description of the Parameter
-     *@return           Description of the Return Value
-     */
-
-    public static Button button( String name, ActionListener listener )
-    {
-        return button( name, null, listener );
-    }
-
-
-    /**
-     *  These functions are for compatibility with the AWT preview window
-     *
-     *@param  name      Description of the Parameter
-     *@param  suffix    Description of the Parameter
-     *@param  listener  Description of the Parameter
-     *@return           Description of the Return Value
-     */
-    public static Button button( String name, String suffix, ActionListener listener )
-    {
-        String command = name;
-        try
-        {
-            command = resources.getString( "button." + name );
-        }
-        catch ( MissingResourceException ex )
-        {
-        }
-        if ( suffix != null )
-            command += suffix;
-        Button b = new Button( command );
-        if ( listener != null )
-        {
-            b.setActionCommand( name );
-            b.addActionListener( listener );
-        }
-        return b;
-    }
-
-
-    /**
-     *  returns a BCheckBox whose label is given by the property
-     *  "checkbox.name". state is the initial state of the checkbox
-     *
-     *@param  name   Description of the Parameter
-     *@param  state  Description of the Parameter
-     *@return        Description of the Return Value
-     */
-    public static BCheckBox bCheckBox( String name, boolean state )
-    {
-        return bCheckBox( name, state, null, null );
-    }
-
 
     /**
      *  Description of the Method
@@ -456,30 +275,6 @@ public class SPMTranslate
     }
 
 
-    /**
-     *  Get the text given by the property "name". If the property is not found,
-     *  this simply uses name. Any occurrances of the patterns "{0}" and "{1}"
-     *  in the text string will be replaced with the strings representations of
-     *  arg1 and arg2, respectively.
-     *
-     *@param  name  Description of the Parameter
-     *@param  arg1  Description of the Parameter
-     *@param  arg2  Description of the Parameter
-     *@return       Description of the Return Value
-     */
-
-    public static String text( String name, Object arg1, Object arg2 )
-    {
-        String pattern = name;
-        try
-        {
-            pattern = resources.getString( "text." + name );
-        }
-        catch ( MissingResourceException ex )
-        {
-        }
-        return MessageFormat.format( pattern, new Object[]{arg1, arg2} );
-    }
 
 
     /**

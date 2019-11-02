bc45a9530009b1d1518f136df5d401782bb4c609
peteihis <peteihis@gmail.com>
2019-11-02 17:03:50 +0200

    white spaces

--- ArtOfIllusion/src/artofillusion/object/Sphere.java
+++ ArtOfIllusion/src/artofillusion/object/Sphere.java
@@ -35,8 +35,8 @@ public class Sphere extends Object3D
   private static double sine[], cosine[];
   private static final Property PROPERTIES[] = new Property [] {
     new Property("X Radius", 0.0, Double.MAX_VALUE, 1.0),
-      new Property("Y Radius", 0.0, Double.MAX_VALUE, 1.0),
-      new Property("Z Radius", 0.0, Double.MAX_VALUE, 1.0)
+    new Property("Y Radius", 0.0, Double.MAX_VALUE, 1.0),
+    new Property("Z Radius", 0.0, Double.MAX_VALUE, 1.0)
   };
 
   static
@@ -115,23 +115,23 @@ public class Sphere extends Object3D
     vert[numvert-2] = new Vec3(0.0, ry, 0.0);
     vert[numvert-1] = new Vec3(0.0, -ry, 0.0);
     for (i = 0; i < SEGMENTS; i++)
+    {
+      from[i] = numvert-2;
+      to[i] = i;
+      for (j = 0; j < SEGMENTS/2-1; j++)
       {
-        from[i] = numvert-2;
-        to[i] = i;
-        for (j = 0; j < SEGMENTS/2-1; j++)
-          {
-            vert[j*SEGMENTS+i] = new Vec3(rx*sine[j+1]*cosine[i], ry*cosine[j+1], rz*sine[j+1]*sine[i]);
-            from[2*(j+1)*SEGMENTS+i] = j*SEGMENTS+i;
-            to[2*(j+1)*SEGMENTS+i] = j*SEGMENTS+(i+1)%SEGMENTS;
-          }
-        for (j = 0; j < SEGMENTS/2-2; j++)
-          {
-            from[(2*j+1)*SEGMENTS+i] = j*SEGMENTS+i;
-            to[(2*j+1)*SEGMENTS+i] = (j+1)*SEGMENTS+i;
-          }
-        from[(2*j+1)*SEGMENTS+i] = j*SEGMENTS+i;
-        to[(2*j+1)*SEGMENTS+i] = numvert-1;
+        vert[j*SEGMENTS+i] = new Vec3(rx*sine[j+1]*cosine[i], ry*cosine[j+1], rz*sine[j+1]*sine[i]);
+        from[2*(j+1)*SEGMENTS+i] = j*SEGMENTS+i;
+        to[2*(j+1)*SEGMENTS+i] = j*SEGMENTS+(i+1)%SEGMENTS;
       }
+      for (j = 0; j < SEGMENTS/2-2; j++)
+      {
+        from[(2*j+1)*SEGMENTS+i] = j*SEGMENTS+i;
+        to[(2*j+1)*SEGMENTS+i] = (j+1)*SEGMENTS+i;
+      }
+      from[(2*j+1)*SEGMENTS+i] = j*SEGMENTS+i;
+      to[(2*j+1)*SEGMENTS+i] = numvert-1;
+    }
     return (cachedWire = new WireframeMesh(vert, from, to));
   }
 
@@ -153,29 +153,29 @@ public class Sphere extends Object3D
     m = 0;
     for (i = 0; i < 8; i++)
       if (i == 0 || i == 2 || i == 5 || i == 7)
+      {
+        for (j = 1; j < size; j++)
         {
-          for (j = 1; j < size; j++)
-            {
-              for (k = 0; k < j-1; k++)
-                {
-                  faces[m++] = new int [] {index[i][j-1][k], index[i][j][k], index[i][j][k+1]};
-                  faces[m++] = new int [] {index[i][j-1][k], index[i][j][k+1], index[i][j-1][k+1]};
-                }
-              faces[m++] = new int [] {index[i][j-1][k], index[i][j][k], index[i][j][k+1]};
-            }
+          for (k = 0; k < j-1; k++)
+          {
+            faces[m++] = new int [] {index[i][j-1][k], index[i][j][k], index[i][j][k+1]};
+            faces[m++] = new int [] {index[i][j-1][k], index[i][j][k+1], index[i][j-1][k+1]};
+          }
+          faces[m++] = new int [] {index[i][j-1][k], index[i][j][k], index[i][j][k+1]};
         }
+      }
       else
+      {
+        for (j = 1; j < size; j++)
         {
-          for (j = 1; j < size; j++)
-            {
-              for (k = 0; k < j-1; k++)
-                {
-                  faces[m++] = new int [] {index[i][j-1][k], index[i][j][k+1], index[i][j][k]};
-                  faces[m++] = new int [] {index[i][j-1][k], index[i][j-1][k+1], index[i][j][k+1]};
-                }
-              faces[m++] = new int [] {index[i][j-1][k], index[i][j][k+1], index[i][j][k]};
-            }
+          for (k = 0; k < j-1; k++)
+          {
+            faces[m++] = new int [] {index[i][j-1][k], index[i][j][k+1], index[i][j][k]};
+            faces[m++] = new int [] {index[i][j-1][k], index[i][j-1][k+1], index[i][j][k+1]};
+          }
+          faces[m++] = new int [] {index[i][j-1][k], index[i][j][k+1], index[i][j][k]};
         }
+      }
     TriangleMesh mesh = new TriangleMesh(vertices, faces);
     mesh.copyTextureAndMaterial(this);
     return mesh;
@@ -198,29 +198,29 @@ public class Sphere extends Object3D
     m = 0;
     for (int i = 0; i < 8; i++)
       if (i == 0 || i == 2 || i == 5 || i == 7)
+      {
+        for (int j = 1; j < size; j++)
         {
-          for (int j = 1; j < size; j++)
-            {
-              for (k = 0; k < j-1; k++)
-                {
-                  tri[m++] = texMapping.mapTriangle(index[i][j-1][k], index[i][j][k], index[i][j][k+1], index[i][j-1][k], index[i][j][k], index[i][j][k+1], vertices);
-                  tri[m++] = texMapping.mapTriangle(index[i][j-1][k], index[i][j][k+1], index[i][j-1][k+1], index[i][j-1][k], index[i][j][k+1], index[i][j-1][k+1], vertices);
-                }
-              tri[m++] = texMapping.mapTriangle(index[i][j-1][k], index[i][j][k], index[i][j][k+1], index[i][j-1][k], index[i][j][k], index[i][j][k+1], vertices);
-            }
+          for (k = 0; k < j-1; k++)
+          {
+            tri[m++] = texMapping.mapTriangle(index[i][j-1][k], index[i][j][k],   index[i][j][k+1],   index[i][j-1][k], index[i][j][k],   index[i][j][k+1],   vertices);
+            tri[m++] = texMapping.mapTriangle(index[i][j-1][k], index[i][j][k+1], index[i][j-1][k+1], index[i][j-1][k], index[i][j][k+1], index[i][j-1][k+1], vertices);
+          }
+          tri[m++] = texMapping.mapTriangle(index[i][j-1][k], index[i][j][k], index[i][j][k+1], index[i][j-1][k], index[i][j][k], index[i][j][k+1], vertices);
         }
+      }
       else
+      {
+        for (int j = 1; j < size; j++)
         {
-          for (int j = 1; j < size; j++)
-            {
-              for (k = 0; k < j-1; k++)
-                {
-                  tri[m++] = texMapping.mapTriangle(index[i][j-1][k], index[i][j][k+1], index[i][j][k], index[i][j-1][k], index[i][j][k+1], index[i][j][k], vertices);
-                  tri[m++] = texMapping.mapTriangle(index[i][j-1][k], index[i][j-1][k+1], index[i][j][k+1], index[i][j-1][k], index[i][j-1][k+1], index[i][j][k+1], vertices);
-                }
-              tri[m++] = texMapping.mapTriangle(index[i][j-1][k], index[i][j][k+1], index[i][j][k], index[i][j-1][k], index[i][j][k+1], index[i][j][k], vertices);
-            }
+          for (k = 0; k < j-1; k++)
+          {
+            tri[m++] = texMapping.mapTriangle(index[i][j-1][k], index[i][j][k+1],   index[i][j][k],   index[i][j-1][k], index[i][j][k+1],   index[i][j][k],   vertices);
+            tri[m++] = texMapping.mapTriangle(index[i][j-1][k], index[i][j-1][k+1], index[i][j][k+1], index[i][j-1][k], index[i][j-1][k+1], index[i][j][k+1], vertices);
+          }
+          tri[m++] = texMapping.mapTriangle(index[i][j-1][k], index[i][j][k+1], index[i][j][k], index[i][j-1][k], index[i][j][k+1], index[i][j][k], vertices);
         }
+      }
     RenderingMesh mesh = new RenderingMesh(vertices, norm, tri, texMapping, matMapping);
     mesh.setParameters(paramValue);
     if (interactive)
@@ -258,108 +258,108 @@ public class Sphere extends Object3D
 
         max = 0.0;
         for (i = 1; i < size; i++)
+        {
+          for (j = 0; j < i-1; j++)
           {
-            for (j = 0; j < i-1; j++)
-              {
-                error = faceError(vert[i-1][j], vert[i][j], vert[i][j+1]);
-                if (error > max)
-                  max = error;
-                error = faceError(vert[i-1][j], vert[i-1][j+1], vert[i][j+1]);
-                if (error > max)
-                  max = error;
-              }
             error = faceError(vert[i-1][j], vert[i][j], vert[i][j+1]);
             if (error > max)
               max = error;
-            if (max > tol)
-              break;
+            error = faceError(vert[i-1][j], vert[i-1][j+1], vert[i][j+1]);
+            if (error > max)
+              max = error;
           }
+          error = faceError(vert[i-1][j], vert[i][j], vert[i][j+1]);
+          if (error > max)
+            max = error;
+          if (max > tol)
+            break;
+        }
         if (max > tol)
-          {
-            // Subdivide the octant of the sphere.
+        {
+          // Subdivide the octant of the sphere.
 
-            newvert = new Vec3 [2*size-1][];
-            for (i = 0; i < 2*size-1; i++)
-              newvert[i] = new Vec3 [i+1];
-            for (i = 0; i < size; i++)
-              for (j = 0; j <= i; j++)
-                newvert[2*i][2*j] = vert[i][j];
-            for (i = 1; i < size; i++)
-              for (j = 0; j < i; j++)
-                {
-                  newvert[2*i][2*j] = vert[i][j];
-                  newvert[2*i-1][2*j] = newVertex(vert[i][j].plus(vert[i-1][j]));
-                  newvert[2*i][2*j+1] = newVertex(vert[i][j].plus(vert[i][j+1]));
-                  newvert[2*i-1][2*j+1] = newVertex(vert[i-1][j].plus(vert[i][j+1]));
-                }
-            vert = newvert;
-            size = 2*size-1;
-          }
+          newvert = new Vec3 [2*size-1][];
+          for (i = 0; i < 2*size-1; i++)
+            newvert[i] = new Vec3 [i+1];
+          for (i = 0; i < size; i++)
+            for (j = 0; j <= i; j++)
+              newvert[2*i][2*j] = vert[i][j];
+          for (i = 1; i < size; i++)
+            for (j = 0; j < i; j++)
+            {
+              newvert[2*i][2*j] = vert[i][j];
+              newvert[2*i-1][2*j] = newVertex(vert[i][j].plus(vert[i-1][j]));
+              newvert[2*i][2*j+1] = newVertex(vert[i][j].plus(vert[i][j+1]));
+              newvert[2*i-1][2*j+1] = newVertex(vert[i-1][j].plus(vert[i][j+1]));
+            }
+          vert = newvert;
+          size = 2*size-1;
+        }
       } while (max > tol);
 
     // Build the complete sphere by mirroring the octant.
 
     v = new Vec3 [4*size*size-8*size+6];
     for (i = 0; i < 8; i++)
-      {
-        index[i] = new int [size][];
-        for (j = 0; j < size; j++)
-          index[i][j] = new int [j+1];
-      }
+    {
+      index[i] = new int [size][];
+      for (j = 0; j < size; j++)
+        index[i][j] = new int [j+1];
+    }
     k = 0;
 
     // First copy all the vertices in the interior of the octant.
 
     for (i = 1; i < size-1; i++)
       for (j = 1; j < i; j++)
-        {
-          v[k] = vert[i][j];
-          index[0][i][j] = k++;
-          v[k] = new Vec3(-vert[i][j].x, vert[i][j].y, vert[i][j].z);
-          index[1][i][j] = k++;
-          v[k] = new Vec3(-vert[i][j].x, -vert[i][j].y, vert[i][j].z);
-          index[2][i][j] = k++;
-          v[k] = new Vec3(vert[i][j].x, -vert[i][j].y, vert[i][j].z);
-          index[3][i][j] = k++;
-          v[k] = new Vec3(vert[i][j].x, vert[i][j].y, -vert[i][j].z);
-          index[4][i][j] = k++;
-          v[k] = new Vec3(-vert[i][j].x, vert[i][j].y, -vert[i][j].z);
-          index[5][i][j] = k++;
-          v[k] = new Vec3(-vert[i][j].x, -vert[i][j].y, -vert[i][j].z);
-          index[6][i][j] = k++;
-          v[k] = new Vec3(vert[i][j].x, -vert[i][j].y, -vert[i][j].z);
-          index[7][i][j] = k++;
-        }
+      {
+        v[k] = vert[i][j];
+        index[0][i][j] = k++;
+        v[k] = new Vec3(-vert[i][j].x, vert[i][j].y, vert[i][j].z);
+        index[1][i][j] = k++;
+        v[k] = new Vec3(-vert[i][j].x, -vert[i][j].y, vert[i][j].z);
+        index[2][i][j] = k++;
+        v[k] = new Vec3(vert[i][j].x, -vert[i][j].y, vert[i][j].z);
+        index[3][i][j] = k++;
+        v[k] = new Vec3(vert[i][j].x, vert[i][j].y, -vert[i][j].z);
+        index[4][i][j] = k++;
+        v[k] = new Vec3(-vert[i][j].x, vert[i][j].y, -vert[i][j].z);
+        index[5][i][j] = k++;
+        v[k] = new Vec3(-vert[i][j].x, -vert[i][j].y, -vert[i][j].z);
+        index[6][i][j] = k++;
+        v[k] = new Vec3(vert[i][j].x, -vert[i][j].y, -vert[i][j].z);
+        index[7][i][j] = k++;
+      }
 
     // Copy the vertices on the border between two octants.
 
     for (i = 1; i < size-1; i++)
-      {
-        v[k] = vert[i][0];
-        index[0][i][0] = index[4][i][0] = k++;
-        v[k] = new Vec3(-vert[i][0].x, vert[i][0].y, vert[i][0].z);
-        index[1][i][0] = index[5][i][0] = k++;
-        v[k] = new Vec3(vert[i][0].x, -vert[i][0].y, vert[i][0].z);
-        index[3][i][0] = index[7][i][0] = k++;
-        v[k] = new Vec3(-vert[i][0].x, -vert[i][0].y, vert[i][0].z);
-        index[2][i][0] = index[6][i][0] = k++;
-        v[k] = vert[size-1][i];
-        index[0][size-1][i] = index[1][size-1][i] = k++;
-        v[k] = new Vec3(vert[size-1][i].x, -vert[size-1][i].y, vert[size-1][i].z);
-        index[2][size-1][i] = index[3][size-1][i] = k++;
-        v[k] = new Vec3(vert[size-1][i].x, vert[size-1][i].y, -vert[size-1][i].z);
-        index[4][size-1][i] = index[5][size-1][i] = k++;
-        v[k] = new Vec3(vert[size-1][i].x, -vert[size-1][i].y, -vert[size-1][i].z);
-        index[6][size-1][i] = index[7][size-1][i] = k++;
-        v[k] = vert[i][i];
-        index[0][i][i] = index[3][i][i] = k++;
-        v[k] = new Vec3(-vert[i][i].x, vert[i][i].y, vert[i][i].z);
-        index[1][i][i] = index[2][i][i] = k++;
-        v[k] = new Vec3(vert[i][i].x, vert[i][i].y, -vert[i][i].z);
-        index[4][i][i] = index[7][i][i] = k++;
-        v[k] = new Vec3(-vert[i][i].x, vert[i][i].y, -vert[i][i].z);
-        index[5][i][i] = index[6][i][i] = k++;
-      }
+    {
+      v[k] = vert[i][0];
+      index[0][i][0] = index[4][i][0] = k++;
+      v[k] = new Vec3(-vert[i][0].x, vert[i][0].y, vert[i][0].z);
+      index[1][i][0] = index[5][i][0] = k++;
+      v[k] = new Vec3(vert[i][0].x, -vert[i][0].y, vert[i][0].z);
+      index[3][i][0] = index[7][i][0] = k++;
+      v[k] = new Vec3(-vert[i][0].x, -vert[i][0].y, vert[i][0].z);
+      index[2][i][0] = index[6][i][0] = k++;
+      v[k] = vert[size-1][i];
+      index[0][size-1][i] = index[1][size-1][i] = k++;
+      v[k] = new Vec3(vert[size-1][i].x, -vert[size-1][i].y, vert[size-1][i].z);
+      index[2][size-1][i] = index[3][size-1][i] = k++;
+      v[k] = new Vec3(vert[size-1][i].x, vert[size-1][i].y, -vert[size-1][i].z);
+      index[4][size-1][i] = index[5][size-1][i] = k++;
+      v[k] = new Vec3(vert[size-1][i].x, -vert[size-1][i].y, -vert[size-1][i].z);
+      index[6][size-1][i] = index[7][size-1][i] = k++;
+      v[k] = vert[i][i];
+      index[0][i][i] = index[3][i][i] = k++;
+      v[k] = new Vec3(-vert[i][i].x, vert[i][i].y, vert[i][i].z);
+      index[1][i][i] = index[2][i][i] = k++;
+      v[k] = new Vec3(vert[i][i].x, vert[i][i].y, -vert[i][i].z);
+      index[4][i][i] = index[7][i][i] = k++;
+      v[k] = new Vec3(-vert[i][i].x, vert[i][i].y, -vert[i][i].z);
+      index[5][i][i] = index[6][i][i] = k++;
+    }
 
     // Finally, copy the vertices at the intersection of four octants.
 
@@ -413,10 +413,10 @@ public class Sphere extends Object3D
   {
     Vec3 n[] = new Vec3 [v.length];
     for (int i = 0; i < v.length; i++)
-      {
-        n[i] = new Vec3 (v[i].x/(rx*rx), v[i].y/(ry*ry), v[i].z/(rz*rz));
-        n[i].normalize();
-      }
+    {
+      n[i] = new Vec3 (v[i].x/(rx*rx), v[i].y/(ry*ry), v[i].z/(rz*rz));
+      n[i].normalize();
+    }
     return n;
   }
 
@@ -434,8 +434,12 @@ public class Sphere extends Object3D
     ValueField xField = new ValueField(rx, ValueField.NONNEGATIVE, 5);
     ValueField yField = new ValueField(ry, ValueField.NONNEGATIVE, 5);
     ValueField zField = new ValueField(rz, ValueField.NONNEGATIVE, 5);
-    ComponentsDialog dlg = new ComponentsDialog(parent.getFrame(), Translate.text("editSphereTitle"),
-      new Widget [] {xField, yField, zField}, new String [] {"X", "Y", "Z"});
+    ComponentsDialog dlg = new ComponentsDialog(
+      parent.getFrame(), 
+      Translate.text("editSphereTitle"),
+      new Widget [] {xField, yField, zField}, 
+      new String [] {"X", "Y", "Z"}
+    );
     if (!dlg.clickedOk())
       return;
     setSize(2.0*xField.getValue(), 2.0*yField.getValue(), 2.0*zField.getValue());
@@ -517,7 +521,6 @@ public class Sphere extends Object3D
   public void applyPoseKeyframe(Keyframe k)
   {
     VectorKeyframe key = (VectorKeyframe) k;
-
     setSize(2.0*key.x, 2.0*key.y, 2.0*key.z);
   }
 
@@ -528,8 +531,10 @@ public class Sphere extends Object3D
   public void configurePoseTrack(PoseTrack track)
   {
     track.setGraphableValues(new String [] {"X Radius", "Y Radius", "Z Radius"},
-        new double [] {rx, ry, rz},
-        new double [][] {{0.0, Double.MAX_VALUE}, {0.0, Double.MAX_VALUE}, {0.0, Double.MAX_VALUE}});
+                             new double [] {rx, ry, rz},
+                             new double [][] {{0.0, Double.MAX_VALUE},
+                                              {0.0, Double.MAX_VALUE}, 
+                                              {0.0, Double.MAX_VALUE}});
   }
 
   /* Allow the user to edit a keyframe returned by getPoseKeyframe(). */
@@ -541,8 +546,12 @@ public class Sphere extends Object3D
     ValueField xField = new ValueField(key.x, ValueField.NONNEGATIVE, 5);
     ValueField yField = new ValueField(key.y, ValueField.NONNEGATIVE, 5);
     ValueField zField = new ValueField(key.z, ValueField.NONNEGATIVE, 5);
-    ComponentsDialog dlg = new ComponentsDialog(parent.getFrame(), Translate.text("editSphereTitle"),
-      new Widget [] {xField, yField, zField}, new String [] {"X", "Y", "Z"});
+    ComponentsDialog dlg = new ComponentsDialog(
+      parent.getFrame(), 
+      Translate.text("editSphereTitle"),
+      new Widget [] {xField, yField, zField}, 
+      new String [] {"X", "Y", "Z"}
+    );
     if (!dlg.clickedOk())
       return;
     key.set(xField.getValue(), yField.getValue(), zField.getValue());

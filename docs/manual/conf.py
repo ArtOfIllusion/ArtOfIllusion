5225abdf6fb9c45e6a5564233a48b93d69030e11
Lucas <lukejsails@gmail.com>
2020-03-21 14:39:21 -0500

    Change to full TOC in manual sidebar

--- docs/manual/conf.py
+++ docs/manual/conf.py
@@ -95,7 +95,7 @@ html_static_path = ['_static']
 # default: ``['localtoc.html', 'relations.html', 'sourcelink.html',
 # 'searchbox.html']``.
 #
-# html_sidebars = {}
+html_sidebars = {'**': ['globaltoc.html', 'relations.html', 'searchbox.html']}
 
 
 # -- Options for HTMLHelp output ---------------------------------------------

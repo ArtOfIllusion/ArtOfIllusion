6989d3d0dec12234486d0f6f4f08a00dbe5b48c2
maxtemplaar@gmail.com <maxtemplaar@gmail.com>
2020-05-23 12:41:30 +0300

    Copyright header update

--- Tests/src/artofillusion/animation/distortion/CustomDistortionTrackTest.java
+++ Tests/src/artofillusion/animation/distortion/CustomDistortionTrackTest.java
@@ -1,8 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+/* Copyright (C) 2020 by Maksim Khramov
+
+   This program is free software; you can redistribute it and/or modify it under the
+   terms of the GNU General Public License as published by the Free Software
+   Foundation; either version 2 of the License, or (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful, but WITHOUT ANY 
+   WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
+   PARTICULAR PURPOSE.  See the GNU General Public License for more details. */
+
 package artofillusion.animation.distortion;
 
 import artofillusion.math.CoordinateSystem;

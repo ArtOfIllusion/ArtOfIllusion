de7e6772b174138e65f19933654dced43970e750
maxtemplaar@gmail.com <maxtemplaar@gmail.com>
2020-10-01 10:56:35 +0300

    Small clean-up

--- ArtOfIllusion/src/artofillusion/procedural/ModuleMenu.java
+++ ArtOfIllusion/src/artofillusion/procedural/ModuleMenu.java
@@ -1,4 +1,5 @@
 /* Copyright (C) 2011 by Peter Eastman
+   Changes copyright (C) 2020 by Maksim Khramov
 
    This program is free software; you can redistribute it and/or modify it under the
    terms of the GNU General Public License as published by the Free Software
@@ -30,12 +31,15 @@ import java.util.List;
 public class ModuleMenu extends CustomWidget
 {
   private final ProcedureEditor editor;
-  private final ArrayList<Category> categories;
+  private final List<Category> categories = new ArrayList<>();
   private final double expandedFraction[];
   private int expandedCategory;
   private Module newModule;
   private boolean isDraggingModule;
 
+  private final Color categoryColor = new Color(0.9f, 0.9f, 0.9f);
+  private final Color selectedCategoryColor = new Color(0.7f, 0.7f, 1.0f);
+  
   public ModuleMenu(ProcedureEditor editor)
   {
     this.editor = editor;
@@ -43,96 +47,89 @@ public class ModuleMenu extends CustomWidget
     addEventLink(MousePressedEvent.class, this, "mousePressed");
     addEventLink(MouseDraggedEvent.class, this, "mouseDragged");
     addEventLink(MouseReleasedEvent.class, this, "mouseReleased");
-    categories = new ArrayList<Category>();
+    
     Category category;
-    categories.add(category = new Category(Translate.text("menu.values")));
-    category.add(new Entry(Translate.text("menu.numberModule"), NumberModule.class));
-    category.add(new Entry(Translate.text("menu.colorModule"), ColorModule.class));
-    category.add(new Entry(Translate.text("menu.xModule"), CoordinateModule.class, null, CoordinateModule.X));
-    category.add(new Entry(Translate.text("menu.yModule"), CoordinateModule.class, null, CoordinateModule.Y));
-    category.add(new Entry(Translate.text("menu.zModule"), CoordinateModule.class, null, CoordinateModule.Z));
-    category.add(new Entry(Translate.text("menu.timeModule"), CoordinateModule.class, null, CoordinateModule.T));
+    categories.add(category = new Category("menu.values"));
+    category.add(new Entry("menu.numberModule", NumberModule.class));
+    category.add(new Entry("menu.colorModule", ColorModule.class));
+    category.add(new Entry("menu.xModule", CoordinateModule.class, null, CoordinateModule.X));
+    category.add(new Entry("menu.yModule", CoordinateModule.class, null, CoordinateModule.Y));
+    category.add(new Entry("menu.zModule", CoordinateModule.class, null, CoordinateModule.Z));
+    category.add(new Entry("menu.timeModule", CoordinateModule.class, null, CoordinateModule.T));
     if (editor.getOwner().allowViewAngle())
-      category.add(new Entry(Translate.text("menu.viewAngleModule"), ViewAngleModule.class));
+      category.add(new Entry("menu.viewAngleModule", ViewAngleModule.class));
     if (editor.getOwner().allowParameters())
-      category.add(new Entry(Translate.text("menu.parameterModule"), ParameterModule.class));
-    category.add(new Entry(Translate.text("menu.commentModule"), CommentModule.class));
+      category.add(new Entry("menu.parameterModule", ParameterModule.class));
+    category.add(new Entry("menu.commentModule", CommentModule.class));
 
-    categories.add(category = new Category(Translate.text("menu.operators")));
-    category.add(new Entry(Translate.text("menu.addModule"), SumModule.class));
-    category.add(new Entry(Translate.text("menu.subtractModule"), DifferenceModule.class));
-    category.add(new Entry(Translate.text("menu.multiplyModule"), ProductModule.class));
-    category.add(new Entry(Translate.text("menu.divideModule"), RatioModule.class));
-    category.add(new Entry(Translate.text("menu.powModule"), PowerModule.class));
-    category.add(new Entry(Translate.text("menu.modModule"), ModModule.class));
-    category.add(new Entry(Translate.text("menu.greaterThanModule"), CompareModule.class));
-    category.add(new Entry(Translate.text("menu.minModule"), MinModule.class));
-    category.add(new Entry(Translate.text("menu.maxModule"), MaxModule.class));
+    categories.add(category = new Category("menu.operators"));
+    category.add(new Entry("menu.addModule", SumModule.class));
+    category.add(new Entry("menu.subtractModule", DifferenceModule.class));
+    category.add(new Entry("menu.multiplyModule", ProductModule.class));
+    category.add(new Entry("menu.divideModule", RatioModule.class));
+    category.add(new Entry("menu.powModule", PowerModule.class));
+    category.add(new Entry("menu.modModule", ModModule.class));
+    category.add(new Entry("menu.greaterThanModule", CompareModule.class));
+    category.add(new Entry("menu.minModule", MinModule.class));
+    category.add(new Entry("menu.maxModule", MaxModule.class));
 
-    categories.add(category = new Category(Translate.text("menu.functions")));
-    category.add(new Entry(Translate.text("menu.expressionModule"), ExprModule.class));
-    category.add(new Entry(Translate.text("menu.customFunctionModule"), FunctionModule.class));
-    category.add(new Entry(Translate.text("menu.scaleShiftModule"), ScaleShiftModule.class));
-    category.add(new Entry(Translate.text("menu.absModule"), AbsModule.class));
-    category.add(new Entry(Translate.text("menu.blurModule"), BlurModule.class));
-    category.add(new Entry(Translate.text("menu.clipModule"), ClipModule.class));
-    category.add(new Entry(Translate.text("menu.interpolateModule"), InterpModule.class));
-    category.add(new Entry(Translate.text("menu.sineModule"), SineModule.class));
-    category.add(new Entry(Translate.text("menu.cosineModule"), CosineModule.class));
-    category.add(new Entry(Translate.text("menu.sqrtModule"), SqrtModule.class));
-    category.add(new Entry(Translate.text("menu.expModule"), ExpModule.class));
-    category.add(new Entry(Translate.text("menu.logModule"), LogModule.class));
-    category.add(new Entry(Translate.text("menu.biasModule"), BiasModule.class));
-    category.add(new Entry(Translate.text("menu.gainModule"), GainModule.class));
-    category.add(new Entry(Translate.text("menu.randomModule"), RandomModule.class));
+    categories.add(category = new Category("menu.functions"));
+    category.add(new Entry("menu.expressionModule", ExprModule.class));
+    category.add(new Entry("menu.customFunctionModule", FunctionModule.class));
+    category.add(new Entry("menu.scaleShiftModule", ScaleShiftModule.class));
+    category.add(new Entry("menu.absModule", AbsModule.class));
+    category.add(new Entry("menu.blurModule", BlurModule.class));
+    category.add(new Entry("menu.clipModule", ClipModule.class));
+    category.add(new Entry("menu.interpolateModule", InterpModule.class));
+    category.add(new Entry("menu.sineModule", SineModule.class));
+    category.add(new Entry("menu.cosineModule", CosineModule.class));
+    category.add(new Entry("menu.sqrtModule", SqrtModule.class));
+    category.add(new Entry("menu.expModule", ExpModule.class));
+    category.add(new Entry("menu.logModule", LogModule.class));
+    category.add(new Entry("menu.biasModule", BiasModule.class));
+    category.add(new Entry("menu.gainModule", GainModule.class));
+    category.add(new Entry("menu.randomModule", RandomModule.class));
 
-    categories.add(category = new Category(Translate.text("menu.colorFunctions")));
-    category.add(new Entry(Translate.text("menu.customColorFunctionModule"), SpectrumModule.class));
-    category.add(new Entry(Translate.text("menu.blendModule"), BlendModule.class));
-    category.add(new Entry(Translate.text("menu.addColorModule"), ColorSumModule.class));
-    category.add(new Entry(Translate.text("menu.subtractColorModule"), ColorDifferenceModule.class));
-    category.add(new Entry(Translate.text("menu.multiplyColorModule"), ColorProductModule.class));
-    category.add(new Entry(Translate.text("menu.lighterModule"), ColorLightenModule.class));
-    category.add(new Entry(Translate.text("menu.darkerModule"), ColorDarkenModule.class));
-    category.add(new Entry(Translate.text("menu.scaleColorModule"), ColorScaleModule.class));
-    category.add(new Entry(Translate.text("menu.RGBModule"), RGBModule.class));
-    category.add(new Entry(Translate.text("menu.HSVModule"), HSVModule.class));
-    category.add(new Entry(Translate.text("menu.HLSModule"), HLSModule.class));
+    categories.add(category = new Category("menu.colorFunctions"));
+    category.add(new Entry("menu.customColorFunctionModule", SpectrumModule.class));
+    category.add(new Entry("menu.blendModule", BlendModule.class));
+    category.add(new Entry("menu.addColorModule", ColorSumModule.class));
+    category.add(new Entry("menu.subtractColorModule", ColorDifferenceModule.class));
+    category.add(new Entry("menu.multiplyColorModule", ColorProductModule.class));
+    category.add(new Entry("menu.lighterModule", ColorLightenModule.class));
+    category.add(new Entry("menu.darkerModule", ColorDarkenModule.class));
+    category.add(new Entry("menu.scaleColorModule", ColorScaleModule.class));
+    category.add(new Entry("menu.RGBModule", RGBModule.class));
+    category.add(new Entry("menu.HSVModule", HSVModule.class));
+    category.add(new Entry("menu.HLSModule", HLSModule.class));
 
-    categories.add(category = new Category(Translate.text("menu.transforms")));
-    category.add(new Entry(Translate.text("menu.linearModule"), TransformModule.class));
-    category.add(new Entry(Translate.text("menu.polarModule"), PolarModule.class));
-    category.add(new Entry(Translate.text("menu.sphericalModule"), SphericalModule.class));
-    category.add(new Entry(Translate.text("menu.jitterModule"), JitterModule.class));
+    categories.add(category = new Category("menu.transforms"));
+    category.add(new Entry("menu.linearModule", TransformModule.class));
+    category.add(new Entry("menu.polarModule", PolarModule.class));
+    category.add(new Entry("menu.sphericalModule", SphericalModule.class));
+    category.add(new Entry("menu.jitterModule", JitterModule.class));
 
-    categories.add(category = new Category(Translate.text("menu.patterns")));
-    category.add(new Entry(Translate.text("menu.noiseModule"), NoiseModule.class));
-    category.add(new Entry(Translate.text("menu.turbulenceModule"), TurbulenceModule.class));
-    category.add(new Entry(Translate.text("menu.gridModule"), GridModule.class));
-    category.add(new Entry(Translate.text("menu.cellsModule"), CellsModule.class));
-    category.add(new Entry(Translate.text("menu.marbleModule"), MarbleModule.class));
-    category.add(new Entry(Translate.text("menu.woodModule"), WoodModule.class));
-    category.add(new Entry(Translate.text("menu.checkerModule"), CheckerModule.class));
-    category.add(new Entry(Translate.text("menu.bricksModule"), BrickModule.class));
-    category.add(new Entry(Translate.text("menu.imageModule"), ImageModule.class));
-
+    categories.add(category = new Category("menu.patterns"));
+    category.add(new Entry("menu.noiseModule", NoiseModule.class));
+    category.add(new Entry("menu.turbulenceModule", TurbulenceModule.class));
+    category.add(new Entry("menu.gridModule", GridModule.class));
+    category.add(new Entry("menu.cellsModule", CellsModule.class));
+    category.add(new Entry("menu.marbleModule", MarbleModule.class));
+    category.add(new Entry("menu.woodModule", WoodModule.class));
+    category.add(new Entry("menu.checkerModule", CheckerModule.class));
+    category.add(new Entry("menu.bricksModule", BrickModule.class));
+    category.add(new Entry("menu.imageModule", ImageModule.class));
+    
     List<Module> plugins = PluginRegistry.getPlugins(Module.class);
-    if (plugins.size() > 0)
+    if (!plugins.isEmpty())
     {
-      categories.add(category = new Category(Translate.text("menu.plugins")));
-      for (int i = 0; i < plugins.size(); i++)
-      {
-        try
-        {
-          Class<? extends Module> moduleClass = plugins.get(i).getClass();
-          category.add(new Entry((moduleClass.newInstance()).getName(), moduleClass, new Object[0]));
-        }
-        catch (Exception ex)
-        {
-          ex.printStackTrace();
-        }
-      }
+      final Category uncategorized = new Category("menu.plugins");
+      categories.add(uncategorized);
+      plugins.forEach(module -> {
+          uncategorized.add(new Entry(module.getName(), module.getClass()));
+      });
     }
+    
     expandedFraction = new double[categories.size()];
     expandedFraction[expandedCategory] = 1.0;
 
@@ -146,19 +143,19 @@ public class ModuleMenu extends CustomWidget
       for (Entry entry : cat.entries)
         width = Math.max(width, metrics.stringWidth(entry.name));
     }
-    setPreferredSize(new Dimension(width+20, 50));
+    setPreferredSize(new Dimension(width + 20, 50));
   }
 
   private void paint(RepaintEvent ev)
   {
     Graphics2D g = ev.getGraphics();
-    FontMetrics metrics = Toolkit.getDefaultToolkit().getFontMetrics(editor.getFont());
+    
+    FontMetrics metrics = g.getFontMetrics(editor.getFont());
     int y = 0;
     int categoryHeight = metrics.getAscent()+metrics.getDescent()+10;
     int entryHeight = metrics.getAscent()+metrics.getDescent()+5;
     int width = getBounds().width;
-    final Color categoryColor = new Color(0.9f, 0.9f, 0.9f);
-    Color selectedCategoryColor = new Color(0.7f, 0.7f, 1.0f);
+
 
     // Loop over categories.
 
@@ -312,13 +309,12 @@ public class ModuleMenu extends CustomWidget
   private static class Category
   {
     public String name;
-    public ArrayList<Entry> entries;
+    public ArrayList<Entry> entries = new ArrayList<>();
     public Rectangle bounds;
 
     public Category(String name)
     {
-      this.name = name;
-      entries = new ArrayList<Entry>();
+      this.name = Translate.text(name);
     }
 
     public void add(Entry entry)
@@ -336,14 +332,12 @@ public class ModuleMenu extends CustomWidget
 
     public Entry(String name, Class moduleClass)
     {
-      this.name = name;
-      this.moduleClass = moduleClass;
-      args = new Object[] {null};
+      this(name, moduleClass, new Object[] {new Point()});
     }
 
     public Entry(String name, Class<? extends Module> moduleClass, Object... args)
     {
-      this.name = name;
+      this.name = Translate.text(name);
       this.moduleClass = moduleClass;
       this.args = args;
     }

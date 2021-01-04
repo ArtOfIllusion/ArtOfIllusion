0bc7801c07dc44fbf408414db34debe15f920ecb
Lucas <lukejsails@gmail.com>
2021-01-03 21:26:35 -0600

    Streamline Linux installer
    - Removed InfoPanel as most contents were repeated on packsPanel
    - Desktop Utility Scripts only installed when Integration pack
      selected, which allows:
    - Replace script based customization of desktop spec with IZPack
      variable expansion
    - Replace HelloPanel with Splash Screen
    - Custom frame label
    - Uninstaller removes desktop integrations

--- InstallerSrc/utils/freedesktop-install.sh
+++ InstallerSrc/utils/freedesktop-install.sh
@@ -3,8 +3,6 @@ echo "Installing Gnome/KDE Shortcuts..." >> $1/install.log
 sh $1/utils/xdg-utils/scripts/xdg-desktop-icon install $1/utils/aoi.desktop --novendor 2>> $1/install.log
 sh $1/utils/xdg-utils/scripts/xdg-mime install $1/utils/aoi.xml --novendor 2>> $1/install.log
 sh $1/utils/xdg-utils/scripts/xdg-mime default aoi.desktop application/x-aoi 2>> $1/install.log
-#sed -e "s%aoi.sh%$1/aoi.sh%" $1/utils/aoi.desktop > $1/utils/tmp 2>> $1/install.log
-#sed -e "s%USER_HOME%$2%" $1/utils/tmp > $1/utils/aoi.desktop 2>> $1/install.log
 sh $1/utils/xdg-utils/scripts/xdg-desktop-menu install $1/utils/aoi.desktop --novendor 2>> $1/install.log
 sh $1/utils/xdg-utils/scripts/xdg-icon-resource install --size 64 $1/utils/icons/64x64/aoi.png --novendor 2>> $1/install.log
 sh $1/utils/xdg-utils/scripts/xdg-icon-resource install --size 48 $1/utils/icons/48x48/aoi.png --novendor 2>> $1/install.log

b2404c0dcc260b584a8b85662a481ed0baf2eb67
maxtemplaar@gmail.com <maxtemplaar@gmail.com>
2022-01-24 15:04:02 +0300

    Lathe tool i18n

--- Tools/src/artofillusion/tools/LatheDialog.java
+++ Tools/src/artofillusion/tools/LatheDialog.java
@@ -1,5 +1,5 @@
 /* Copyright (C) 2001-2007 by Peter Eastman
-   Changes copyright (C) 2020 by Maksim Khramov
+   Changes copyright (C) 2020-2022 by Maksim Khramov
 
    This program is free software; you can redistribute it and/or modify it under the
    terms of the GNU General Public License as published by the Free Software
@@ -31,14 +31,14 @@ public class LatheDialog extends BDialog
   BRadioButton xBox, yBox, zBox, endsBox;
   ValueField radiusField, segmentsField;
   ValueSlider angleSlider;
-  BButton okButton, cancelButton;
+
   ObjectPreviewCanvas preview;
 
   private static int counter = 1;
 
   public LatheDialog(LayoutWindow window, ObjectInfo curve)
   {
-    super(window, "Lathe", true);
+    super(window, Translate.text("stdtools:lathe.dialog.name"), true);
     this.window = window;
     theCurve = (Curve) curve.getObject();
     curveInfo = curve;
@@ -48,18 +48,18 @@ public class LatheDialog extends BDialog
     FormContainer content = new FormContainer(3, 10);
     setContent(BOutline.createEmptyBorder(content, UIUtilities.getStandardDialogInsets()));
     content.setDefaultLayout(new LayoutInfo(LayoutInfo.WEST, LayoutInfo.NONE, new Insets(0, 0, 0, 5), null));
-    content.add(new BLabel("Select Lathe Axis:"), 0, 0, 2, 1);
+    content.add(Translate.label("stdtools:lathe.axis.select.label"), 0, 0, 2, 1);
     axisGroup = new RadioButtonGroup();
-    content.add(xBox = new BRadioButton("X axis", false, axisGroup), 0, 1, 2, 1);
-    content.add(yBox = new BRadioButton("Y axis", true, axisGroup), 0, 2, 2, 1);
-    content.add(zBox = new BRadioButton("Z axis", false, axisGroup), 0, 3, 2, 1);
-    content.add(endsBox = new BRadioButton("Line through endpoints", false, axisGroup), 0, 4, 2, 1);
+    content.add(xBox = new BRadioButton(Translate.text("stdtools:lathe.rotate.xaxis"), false, axisGroup), 0, 1, 2, 1);
+    content.add(yBox = new BRadioButton(Translate.text("stdtools:lathe.rotate.yaxis"), true, axisGroup), 0, 2, 2, 1);
+    content.add(zBox = new BRadioButton(Translate.text("stdtools:lathe.rotate.zaxis"), false, axisGroup), 0, 3, 2, 1);
+    content.add(endsBox = new BRadioButton(Translate.text("stdtools:lathe.rotate.endpoints"), false, axisGroup), 0, 4, 2, 1);
     axisGroup.addEventLink(SelectionChangedEvent.class, this, "makeObject");
-    content.add(new BLabel("Total Rotation Angle:"), 0, 5, 2, 1);
+    content.add(Translate.label("stdtools:lathe.rotation.angle.label"), 0, 5, 2, 1);
     content.add(angleSlider = new ValueSlider(0.0, 360.0, 180, 360.0), 0, 6, 2, 1);
     angleSlider.addEventLink(ValueChangedEvent.class, this, "makeObject");
-    content.add(new BLabel("Radius:"), 0, 7);
-    content.add(new BLabel("Segments:"), 0, 8);
+    content.add(Translate.label("stdtools:lathe.radius.label"), 0, 7);
+    content.add(Translate.label("stdtools:lathe.segments.label"), 0, 8);
     content.add(radiusField = new ValueField(0.0, ValueField.NONE), 1, 7);
     content.add(segmentsField = new ValueField(8.0, ValueField.POSITIVE+ValueField.INTEGER), 1, 8);
     radiusField.addEventLink(ValueChangedEvent.class, this, "makeObject");
@@ -73,8 +73,8 @@ public class LatheDialog extends BDialog
     // Add the buttons at the bottom.
 
     RowContainer buttons = new RowContainer();
-    buttons.add(okButton = Translate.button("ok", this, "doOk"));
-    buttons.add(cancelButton = Translate.button("cancel", this, "dispose"));
+    buttons.add(Translate.button("ok", this, "doOk"));
+    buttons.add(Translate.button("cancel", this, "dispose"));
     content.add(buttons, 0, 9, 3, 1, new LayoutInfo());
     selectDefaults();
     makeObject();
@@ -88,7 +88,7 @@ public class LatheDialog extends BDialog
     CoordinateSystem coords = curveInfo.getCoords().duplicate();
     Vec3 offset = curveInfo.getCoords().fromLocal().times(theCurve.getVertices()[0].r).minus(coords.fromLocal().times(((Mesh) preview.getObject().getObject()).getVertices()[0].r));
     coords.setOrigin(coords.getOrigin().plus(offset));
-    window.addObject(preview.getObject().getObject(), coords, "Lathed Object "+(counter++), null);
+    window.addObject(preview.getObject().getObject(), coords, "Lathed Object " + (counter++), null);
     window.setSelection(window.getScene().getNumObjects()-1);
     window.setUndoRecord(new UndoRecord(window, false, UndoRecord.DELETE_OBJECT, window.getScene().getNumObjects()-1));
     window.updateImage();

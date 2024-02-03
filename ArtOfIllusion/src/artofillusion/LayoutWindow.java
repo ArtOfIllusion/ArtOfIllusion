7d1598552ae50072b928ff2517a3bf49a66ca400
makiam <maxtemplaar@gmail.com>
2024-02-03 14:43:47 +0300

    Replace reflection-style commands binding with lambdas

--- ArtOfIllusion/src/artofillusion/LayoutWindow.java
+++ ArtOfIllusion/src/artofillusion/LayoutWindow.java
@@ -803,25 +803,25 @@ public class LayoutWindow extends BFrame implements EditingWindow, PopupMenuMana
   {
     popupMenu = new BPopupMenu();
     popupMenuItem = new BMenuItem [15];
-    popupMenu.add(popupMenuItem[0] = Translate.menuItem("editObject", this, "editObjectCommand", null));
-    popupMenu.add(popupMenuItem[1] = Translate.menuItem("objectLayout", this, "objectLayoutCommand", null));
-    popupMenu.add(popupMenuItem[2] = Translate.menuItem("setTextureAndMaterial", this, "setTextureCommand", null));
-    popupMenu.add(popupMenuItem[3] = Translate.menuItem("renameObject", this, "renameObjectCommand", null));
-    popupMenu.add(popupMenuItem[4] = Translate.menuItem("convertToTriangle", this, "convertToTriangleCommand", null));
+    popupMenu.add(popupMenuItem[0] = Translate.menuItem("editObject", this, "editObjectCommand"));
+    popupMenu.add(popupMenuItem[1] = Translate.menuItem("objectLayout", event -> objectLayoutCommand()));
+    popupMenu.add(popupMenuItem[2] = Translate.menuItem("setTextureAndMaterial", event -> setTextureCommand()));
+    popupMenu.add(popupMenuItem[3] = Translate.menuItem("renameObject", event -> renameObjectCommand()));
+    popupMenu.add(popupMenuItem[4] = Translate.menuItem("convertToTriangle", event -> convertToTriangleCommand()));
     popupMenu.addSeparator();    
-    popupMenu.add(popupMenuItem[5] = Translate.menuItem("selectChildren", this, "actionPerformed", null));
-    popupMenu.add(Translate.menuItem("selectAll", this, "selectAllCommand", null));
-    popupMenu.add(popupMenuItem[6] = Translate.menuItem("deselectAll", this, "clearSelection", null));
+    popupMenu.add(popupMenuItem[5] = Translate.menuItem("selectChildren", this, "actionPerformed"));
+    popupMenu.add(Translate.menuItem("selectAll", event -> selectAllCommand()));
+    popupMenu.add(popupMenuItem[6] = Translate.menuItem("deselectAll", event -> clearSelection()));
     popupMenu.addSeparator();
-    popupMenu.add(popupMenuItem[7] = Translate.menuItem("hideSelection", this, "actionPerformed", null));
-    popupMenu.add(popupMenuItem[8] = Translate.menuItem("showSelection", this, "actionPerformed", null));
+    popupMenu.add(popupMenuItem[7] = Translate.menuItem("hideSelection", this, "actionPerformed"));
+    popupMenu.add(popupMenuItem[8] = Translate.menuItem("showSelection", this, "actionPerformed"));
     popupMenu.add(popupMenuItem[9] = Translate.menuItem("lockSelection", this, "actionPerformed"));
     popupMenu.add(popupMenuItem[10] = Translate.menuItem("unlockSelection", this, "actionPerformed"));
     popupMenu.addSeparator();
-    popupMenu.add(popupMenuItem[11] = Translate.menuItem("cut", this, "cutCommand", null));
-    popupMenu.add(popupMenuItem[12] = Translate.menuItem("copy", this, "copyCommand", null));
-    popupMenu.add(popupMenuItem[13] = Translate.menuItem("paste", this, "pasteCommand", null));
-    popupMenu.add(popupMenuItem[14] = Translate.menuItem("clear", this, "clearCommand", null));
+    popupMenu.add(popupMenuItem[11] = Translate.menuItem("cut", event -> cutCommand()));
+    popupMenu.add(popupMenuItem[12] = Translate.menuItem("copy", event -> copyCommand()));
+    popupMenu.add(popupMenuItem[13] = Translate.menuItem("paste", event -> pasteCommand()));
+    popupMenu.add(popupMenuItem[14] = Translate.menuItem("clear",event -> clearCommand()));
   }
 
   /** Display the popup menu. */
